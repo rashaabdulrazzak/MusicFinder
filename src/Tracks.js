55838a6031d199bc1febb41c3c69abb6979bf80b
rashaabdulrazzak <rasha.abdulrazzak@gmail.com>
2020-05-13 02:54:07 +0300

    enhance styling

--- src/Tracks.js
+++ src/Tracks.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container,Row,Col } from 'react-bootstrap';
+import { Container, Row } from "react-bootstrap";
 
 class Tracks extends Component {
   state = { playing: false, audio: null, playingPreviewUrl: null };
@@ -18,41 +18,45 @@ class Tracks extends Component {
       }
     }
   };
-  trackIcon= track=>{
-      if(!track.preview_url){
-          return <span> N/A</span>
-      }
-      if(this.state.playing && this.state.playingPreviewUrl === track.preview_url){
-          return <span> |  |</span>
-      }
-    return <span>&#9654;</span>
-  }
+  trackIcon = (track) => {
+    if (!track.preview_url) {
+      return <span> N/A</span>;
+    }
+    if (
+      this.state.playing &&
+      this.state.playingPreviewUrl === track.preview_url
+    ) {
+      return <span> | |</span>;
+    }
+    return <span>&#9654;</span>;
+  };
   render() {
     const { tracks } = this.props;
     return (
-        <Container>
-        <Row className='justify-content-center'>           
-        {tracks.map((track) => {
-          const { id, name, album, preview_url } = track;
-          return (              
-            <div className='col-4'
-             key={id}
-             onClick={this.playAudio(preview_url)}
-             >
-             <div className='track'>
-             <img 
-              src={album.images[0].url} 
-              alt="trackimage" 
-              className='track-image'
-               />
-             <p className='track-text'>{name}</p>
-          <p className='track-icon'>{this.trackIcon(track)}</p>
-            </div>
-            </div>
-          );
-        })}
+      <Container>
+        <Row className="justify-content-center">
+          {tracks.map((track) => {
+            const { id, name, album, preview_url } = track;
+            return (
+              <div
+                className="col-sm-12 col-md-6 col-lg-4"
+                key={id}
+                onClick={this.playAudio(preview_url)}
+              >
+                <div className="track">
+                  <img
+                    src={album.images[0].url}
+                    alt="trackimage"
+                    className="track-image"
+                  />
+                  <p className="track-text">{name}</p>
+                  <p className="track-icon">{this.trackIcon(track)}</p>
+                </div>
+              </div>
+            );
+          })}
         </Row>
-        </Container>
+      </Container>
     );
   }
 }
