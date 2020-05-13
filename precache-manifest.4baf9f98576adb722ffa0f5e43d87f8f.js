a88eb2acd9f2ca8eae8a6b0ac25af8dcd4bf5336
rashaabdulrazzak <rasha.abdulrazzak@gmail.com>
2020-05-13 12:41:52 +0300

    Updates

---
+++ precache-manifest.4baf9f98576adb722ffa0f5e43d87f8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60beb8b62de90cdc56a3bf274255c4e8",
+    "url": "/Musicfinder/index.html"
+  },
+  {
+    "revision": "c2cd7877f662695ee5a3",
+    "url": "/Musicfinder/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "e6246ff2f54828a2ae75",
+    "url": "/Musicfinder/static/css/main.f76464f4.chunk.css"
+  },
+  {
+    "revision": "c2cd7877f662695ee5a3",
+    "url": "/Musicfinder/static/js/2.a88633a5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Musicfinder/static/js/2.a88633a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6246ff2f54828a2ae75",
+    "url": "/Musicfinder/static/js/main.537d453a.chunk.js"
+  },
+  {
+    "revision": "2c7faef44f5ad8162c69",
+    "url": "/Musicfinder/static/js/runtime-main.7284155b.js"
+  }
+]);
